728521dc03c8cf750eda4a550915d3c48938b85b
suzhihua <1079087531@qq.com>
2018-08-07 14:09:07 +0800

    add 第18章 Django入门

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('topics', views.topics, name='topics'),
+    path('topic/<int:topic_id>/', views.topic, name='topic')
 ]
